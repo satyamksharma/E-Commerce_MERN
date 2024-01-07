c6a7b3e9accae5d27e8e898c066bdf0400331106
Satyam Kumar <ksatyam433@gmail.com>
2024-01-07 21:57:02 +0530

    add: routing to pages and links

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from 'react-router-dom';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App.jsx';
+import HomeScreen from './screens/HomeScreen.jsx';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route
+            path='/'
+            element={<App />}
+        >
+            <Route
+                path='/'
+                element={<HomeScreen />}
+            />
+        </Route>
+    )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
