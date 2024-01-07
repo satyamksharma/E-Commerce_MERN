418c7c59e005500d0457e3da54a54752668b34e0
Satyam Kumar <ksatyam433@gmail.com>
2024-01-07 18:56:48 +0530

    edit: restructured files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const App = () => {
     return <h1>Welcome to digi store</h1>;
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.jsx';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
 );
-
-reportWebVitals();
