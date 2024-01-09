444333a989aa35bae69cd5ba2e62976e933987cc
Satyam Kumar <ksatyam433@gmail.com>
2024-01-09 22:51:55 +0530

    edit: added api call

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import {
 } from 'react-router-dom';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
-import App from './App.jsx';
+import App from './App';
 import HomeScreen from './screens/HomeScreen.jsx';
 import ProductScreen from './screens/ProductScreen.jsx';
 
