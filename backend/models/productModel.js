78c25598b87a2c5c641377a13b9085e960dd7e9d
Satyam Kumar <ksatyam433@gmail.com>
2024-01-10 00:22:27 +0530

    add: user schema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,5 +1,16 @@
 import mongoose from 'mongoose';
 
+const reviewSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        rating: { type: Number, required: true },
+        comment: { type: String, required: true },
+    },
+    {
+        timestamps: true,
+    }
+);
+
 const productSchema = new mongoose.Schema(
     {
         user: {
